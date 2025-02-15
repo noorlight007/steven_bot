d9890042b4645967a33dcf414988481ebb8cbe47
noorlight007 <contactwnoor@gmail.com>
2025-02-15 17:41:49 +0600

    update

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def callback():
         headers = {"Authorization": f"Bearer {session['access_token']}"}
         live_jobs = requests.get(f"{API_BASE_URL}/jobads?status=current&offset=0&limit=50", headers=headers)
         if live_jobs.status_code == 200:
-            with open("live_jobs.json", "w") as file:
+            with open("./live_jobs.json", "w") as file:
                 json.dump(live_jobs.json(), file, indent=4)
         return {"status": "success"}
     else:
