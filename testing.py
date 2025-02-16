1dd928c7699ebc8648ba83e61ad8f94bbb718f10
noorlight007 <contactwnoor@gmail.com>
2025-02-16 07:02:01 +0600

    update

--- testing.py
+++ testing.py
@@ -2,5 +2,10 @@ import requests
 # print(send_twilio_message())
 sample_jon_id = "591198"
 sample_application_id = "8762438"
-job_details = requests.get(f"https://chatbot.rd1.co.uk/get_job_details/{sample_jon_id}/{sample_application_id}").json()
-print(job_details)
+
+data = {
+    "job_id": "591198",
+    "application_id": "8762438"
+}
+response = requests.post("https://chatbot.rd1.co.uk/get_job_details", json=data)
+print(response.json())
